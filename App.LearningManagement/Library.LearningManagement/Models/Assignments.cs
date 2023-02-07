abc74480f72bccbfb097c080c3bdc7ca40b54580
Joseph Nieves <jdn20u@fsu.edu>
2023-02-07 18:32:54 -0500

    Adding the remaining models to the project. Closes #1 Close#2 Close#5 Close#5

---
+++ App.LearningManagement/Library.LearningManagement/Models/Assignments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.LearningManagement.Models
+{
+    public class Assignments
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal TotalAvaliablePoints { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
